8725d8230e17d23a39e39733c0ba2cf160706ce0
Jaime Toro <zomo505@gmail.com>
2024-12-20 23:53:45 +0100

    added 2 js files for the details txt

---
+++ gMapsAdvancedMarkers/src/components/geoDetails.js
@@ -0,0 +1,7 @@
+export const geocodingDetailsText = `
+  <h3>Geocoding API Details</h3>
+  <p> 
+    This section provides details about the Geocoding API. 
+    [Insert your detailed information here] 
+  </p>
+`;
---
+++ gMapsAdvancedMarkers/src/components/jsDetails.js
@@ -0,0 +1,7 @@
+export const jsDetailsText = `
+  <h3>JavaScript API Details</h3>
+  <p> 
+    This section provides details about the JavaScript API. 
+    [Insert your detailed information here] 
+  </p>
+`;
